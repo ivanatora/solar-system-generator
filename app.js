8d8863c9309bc78ccedee147b182bf793ede8630
ivanatora <ivanatora@gmail.com>
2016-08-23 13:29:45 +0300

    Serving static resources

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.get('/', function(request, response) {
     response.render('pages/index');
 });
 
+app.use(express.static('public'));
+
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port ', app.get('port'));
 });
