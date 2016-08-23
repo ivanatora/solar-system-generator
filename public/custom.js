ee6c111fd4366f028e75ef50b760b5ad3ddb9663
ivanatora <ivanatora@gmail.com>
2016-08-23 15:32:59 +0300

    Have some consideration for Main Sequence and giants

--- public/custom.js
+++ public/custom.js
@@ -1,29 +1,74 @@
 
-function expon(val){
-    var minv = Math.log(gMinPrice);
-    var maxv = Math.log(gMaxPrice);
-    var scale = (maxv-minv) / (gMaxPrice-gMinPrice);
-    return Math.exp(minv + scale*(val-gMinPrice));
-
-}
-function logposition(val){
-    var minv = Math.log(gMinPrice);
-    var maxv = Math.log(gMaxPrice);
-    var scale = (maxv-minv) / (gMaxPrice-gMinPrice);
-    return (Math.log(val)-minv) / scale + gMinPrice;
-}
-
 function calculateTemperature(){
     var iMass = $('input[name="mass"]').val();
     var iTemp = 5740 * Math.pow(iMass, 0.54);
     iTemp = Math.ceil(iTemp / 100) * 100;
+    
+    if (iTemp > 30000){
+        iTemp = Math.ceil(Math.random() * 10000 + 30000);
+    }
     $('input[name="temperature"]').val(iTemp);
     calculateEstimate();
 }
 
-function calculateEstimate(){
-    console.log('estimate?')
+function calculateLumnosity(){
+    var iMass = $('input[name="mass"]').val();
+    var iLumnosity = 0;
+    
+    // equation according to https://en.wikipedia.org/wiki/Mass%E2%80%93luminosity_relation
+    if (iMass <= 0.43){
+        iLumnosity = 0.23 * Math.pow(iMass, 2.3);
+    }
+    else if (iMass > 0.43 && iMass <= 2){
+        iLumnosity = Math.pow(iMass, 4);
+    }
+    else if (iMass > 2 && iMass <= 20){
+        iLumnosity = 1.5 * Math.pow(iMass, 3.5)
+    }
+    else {
+        iLumnosity = 3200 * iMass;
+    }
+    
+    if (iLumnosity > 10){
+        iLumnosity = Math.ceil(iLumnosity);
+    }
+    
+    $('input[name="lumnosity"]').val(iLumnosity);
+    calculateEstimate();
 }
+
+function calculateDensity(){
+    var iMass = parseInt($('input[name="mass"]').val());
+    var iRadius = $('input[name="radius"]').val();
+    var iSunVolumes = Math.pow(iRadius, 3);
+    var iDensity = iMass / iSunVolumes;
+    iDensity = Math.round(iDensity * 10000) / 10000;
+    
+    $('input[name="density"]').val(iDensity);
+}
+
+function calculateEstimate(){
+    var iMass = $('input[name="mass"]').val();
+    var iTemperature = $('input[name="temperature"]').val();
+    var iLumnosity = $('input[name="lumnosity"]').val();
+    var iRadius = $('input[name="radius"]').val();
+    
+    var sEstimate = 'Unknown';
+    
+    // mass check
+    if (iMass < 20){
+        sEstimate = 'Main sequence';
+    }
+    
+    // radius check
+    if (sEstimate === 'Unknown' && iRadius > 10){
+        sEstimate = 'Giant';
+    }
+    
+    $('.estimated').html(sEstimate);
+}
+
+// sliders
 
 $('#mass-slider').slider({
     min: 0.1,
@@ -31,11 +76,17 @@ $('#mass-slider').slider({
     slide: function(event, ui){
         $('input[name="mass"]').val(Math.ceil(ui.value));
         calculateTemperature();
+        calculateLumnosity();
+        calculateDensity();
+        calculateEstimate();
     }
 });
 $('input[name="mass"]').change(function(e){
     $('#mass-slider').slider('value', $(this).val());
     calculateTemperature();
+    calculateLumnosity();
+    calculateDensity();
+    calculateEstimate();
 })
 $('input[name="mass"]').val(1);
 
@@ -44,9 +95,13 @@ $('#radius-slider').slider({
     max: 2000,
     slide: function(event, ui){
         $('input[name="radius"]').val(Math.ceil(ui.value));
+        calculateEstimate();
+        calculateDensity();
     }
 })
 $('input[name="radius"]').change(function(e){
     $('#radius-slider').slider('value', $(this).val());
+    calculateEstimate();
+    calculateDensity();
 })
 $('input[name="radius"]').val(1);
