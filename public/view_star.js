7638c1b16320774b22804a715f800415fdef2377
ivanatora <ivanatora@gmail.com>
2016-08-24 16:38:55 +0300

    Calculating orbital period

--- public/view_star.js
+++ public/view_star.js
@@ -8,6 +8,23 @@ function calculateDensity(){
     $('input[name="density"]').val(iDensity);
 }
 
+function calculateOrbitalPeriod(){
+    var iOrbitalRadius = $('input[name="orbital_radius"]').val();
+    var iORbitalRadiusMeters = iOrbitalRadius * 149597871 * Math.pow(10, 3);
+    var iStarMass = $('.star-mass').html();
+    var iStarMassKg = iStarMass * 1.989 * Math.pow(10, 30); // kgs
+    
+    var gravConst = 6.674 * Math.pow(10, -11);
+    
+    var mu = gravConst * iStarMassKg;
+    
+    var iPeriodSeconds = 2 * Math.PI * Math.sqrt( Math.pow(iORbitalRadiusMeters, 3) / mu);
+    var iPeriodDays = iPeriodSeconds / (3600 * 24);
+    iPeriodDays = parseInt(iPeriodDays);
+    
+    $('input[name="orbital_period"]').val(iPeriodDays);
+}
+
 
 $('#mass-slider').slider({
     min: 0.01,
@@ -41,4 +58,21 @@ $('input[name="radius"]').change(function(e){
 })
 $('input[name="radius"]').val(1);
 
+$('#orbital-radius-slider').slider({
+    min: 0.2,
+    max: 40,
+    step: 0.1,
+    value: 1,
+    slide: function(event, ui){
+        $('input[name="orbital_radius"]').val(ui.value);
+        calculateOrbitalPeriod();
+    }
+})
+$('input[name="orbital_radius"]').change(function(e){
+    $('#orbital-radius-slider').slider('value', $(this).val());
+    calculateOrbitalPeriod();
+})
+$('input[name="orbital_radius"]').val(1);
+
 calculateDensity();
+calculateOrbitalPeriod();
